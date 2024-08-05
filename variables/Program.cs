248d0b096e83f74579a4aa1ad81e8a6034538f7a
Morgoz <morgoz@inbox.ru>
2024-08-05 19:51:10 +0300

    learnt console programming and variables

---
+++ variables/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int Cracker = 0;
+Console.WriteLine(Cracker);
+float myClass = 0.0f;
+Console.WriteLine(myClass);
