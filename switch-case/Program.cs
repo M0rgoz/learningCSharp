befae4b7b9233465bf202d6e2a04501f916a1349
Morgoz <morgoz@inbox.ru>
2024-08-21 18:50:47 +0300

    switch-case added.

---
+++ switch-case/Program.cs
@@ -0,0 +1,30 @@
+﻿int month = DateTime.Now.Month;
+//Console.WriteLine(month);
+//21.08.2024
+
+//int month = 10;
+switch (month)
+{
+    /*case 12 or 1 or 2:
+        Console.WriteLine("Winter");
+        break;*/
+
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Winter");
+        break;
+
+
+    case 3 or 4 or 5:
+        Console.WriteLine("Spring");
+        break;
+    case 6 or 7 or 8:
+        Console.WriteLine("Summer");
+        break;
+    case 9 or 10 or 11:
+        Console.WriteLine("Autumn (Fall)");
+        break;
+    default:
+    break;
+}
