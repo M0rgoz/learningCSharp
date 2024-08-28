1b79b363c8dd8920659f1c48481e49c789549641
Morgoz <morgoz@inbox.ru>
2024-08-28 03:41:32 +0300

    plus-minus part2 added.

--- hackerRank/plus-minus/Program.cs
+++ hackerRank/plus-minus/Program.cs
@@ -34,8 +34,8 @@ foreach (var number in arr)
 Console.WriteLine("The ratio of positive numbers: " + ((double)positive / n).ToString("F6"));
 */
 
-// Answer:
 
+/* 
         int positive = 0;
         int negative = 0;
         int zero = 0;
@@ -53,3 +53,68 @@ Console.WriteLine("The ratio of positive numbers: " + ((double)positive / n).ToS
         Console.WriteLine((double)positive / n).ToString("F6");
         Console.WriteLine((double)negative / n).ToString("F6");
         Console.WriteLine((double)zero / n).ToString("F6");
+*/
+
+
+// Answer:
+
+
+
+
+        using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+        
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        
+        foreach (int number in arr){
+            if(number > 0){
+                positive++;
+            }else if(number < 0){
+                negative++;
+            }else{
+                zero++;
+            }
+        }
+        Console.WriteLine(((double)positive / n).ToString("F6"));
+        Console.WriteLine(((double)negative / n).ToString("F6"));
+        Console.WriteLine(((double)zero / n).ToString("F6"));
+        Result.plusMinus(arr);
+    }
+}
