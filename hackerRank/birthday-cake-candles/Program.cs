0bb019de8bca4032964ab32da95d02cfa2d7bef7
Morgoz <morgoz@inbox.ru>
2024-08-31 04:05:25 +0300

    birthday-cake-candles solved.

---
+++ hackerRank/birthday-cake-candles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution {
+    static void Main(String[] args){
+        Console.ReadLine();
+        var candleHeights = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        var maxHeight = candleHeights.Max();
+        var maxCount = candleHeights.Count(x => x == maxHeight);
+        Console.WriteLine(maxCount);
+    }
+}
+
