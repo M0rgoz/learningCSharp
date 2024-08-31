99f3140586b57f56dc96be2c1e956651dbf240de
Morgoz <morgoz@inbox.ru>
2024-08-31 22:16:55 +0300

    compare-the-triplets solved.

---
+++ hackerRank/compare-the-triplets/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution{
+    static void Main(string[] args){
+        //Console.WriteLine("Enter Bob's array: ");
+        var bobArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        //Console.WriteLine("Enter Alice's array: ");
+        var aliceArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        var bobPoints = 0;
+        var alicePoints = 0;
+        for(int i = 0; i < bobArray.Length; i++){
+            if(bobArray[i] > aliceArray[i]){
+                bobPoints++;
+            }else if(bobArray[i] < aliceArray[i]){
+                alicePoints++;
+            }else{
+                continue;
+            }
+        }
+        Console.WriteLine(bobPoints + " " + alicePoints);
+    }
+}
