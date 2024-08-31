ca67e569d1160805a7a2a95b5ce4f44532924b88
Morgoz <morgoz@inbox.ru>
2024-08-31 04:32:32 +0300

    simple-array-sum solved.

---
+++ hackerRank/simple-array-sum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static void Main(string[] args){
+        Console.ReadLine();
+        var numArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int sum = 0;
+        foreach (var num in numArray){
+            sum += num;
+        }
+        Console.WriteLine(sum);
+    }
+}
