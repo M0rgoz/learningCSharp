07cfcc2891ee0c8f5fcc6b440b3ef275b434be35
Morgoz <morgoz@inbox.ru>
2024-08-31 05:05:32 +0300

    day 7 solved.

---
+++ hackerRank/day7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution{
+    static void Main(string[] args){
+        Console.ReadLine();
+        var array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        Array.Reverse(array);
+        foreach(var i in array){
+            Console.Write(i + " ");
+        }
+    }
+}
