b32f7e4eb43ad2ecf3be63036cca5aba7dde7408
Morgoz <morgoz@inbox.ru>
2024-09-02 09:18:56 +0300

    migratory-birds solved.

---
+++ hackerRank/migratory-birds/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+class Solution {
+    static void Main(String[] args){
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
+        int[] birdCount = new int[n];
+
+        foreach(int bird in arr){
+            birdCount[bird]++;
+        }
+        int maxValue = birdCount.Max();
+        int maxIndex = birdCount.ToList().IndexOf(maxValue);
+        Console.WriteLine(maxIndex);
+    }
+}
