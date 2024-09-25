e0d5be84c6d5cbe4443fee4fed40557bcbdaeae4
Morgoz <morgoz@inbox.ru>
2024-09-25 11:56:28 +0300

    updated

--- recursive-extension-methods/Program.cs
+++ recursive-extension-methods/Program.cs
@@ -6,20 +6,66 @@ public class Program
 static void Main(string[] args){
     //Recursive
     int result = 1;
-    for (int i = 0; i < 5; i++){
+    for (int i = 1; i < 5; i++)
         result = result * 3;
         Console.WriteLine(result);
-        Islemler instance = new Islemler();
-        Console.WriteLine(instance.Expo(3, 4));
+        Islemler instance = new();
+        Console.WriteLine(instance.Expo(3,4));
+    //Extension Methods
+    string expression = "Hello World";
+    bool sonuc = expression.CheckSpaces();
+    Console.WriteLine(sonuc);
+    int [] dizi = {9, 3, 6, 2, 1, 5};
+    dizi.SortArray();
+    dizi.WriteOnScreen();
+    if(sonuc)
+    {
+        Console.WriteLine(expression.RemoveSpaces());
+        Console.WriteLine(expression.MakeUpperCase());
+        Console.WriteLine(expression.MakeLowerCase());
     }
+
 }
 }
 public class Islemler{
-    public int Expo(int sayi, int us){
-        if (us < 2){
+    public int Expo(int sayi, int us)
+    {
+        if (us < 2)
             return sayi;
-        }
-        return sayi * Expo(sayi, us - 1);
+        return Expo(sayi,us-1)*sayi;
+    }
+}
+
+public static class Extension
+{
+    public static bool CheckSpaces(this string param)
+    {
+        return param.Contains(" ");
+    }
+
+    public static string RemoveSpaces(this string param)
+    {
+        string[] arr = param.Split(" ");
+        return string.Join("", arr);
+    }
+
+    public static string MakeUpperCase(this string param)
+    {
+        return param.ToUpper();
+    }
+    public static string MakeLowerCase(this string param)
+    {
+        return param.ToLower();
+    }   
+    public static int[] SortArray(this int[] param)
+    {
+        Array.Sort(param);
+        return param;
+    }
+    public static void WriteOnScreen(this int[] param)
+    {
+        foreach(int item in param)
+        Console.WriteLine(item);
     }
 }
 
